32bc839f5a0e3c8fe8b81cd65be9715df01155aa
Victoria Ng <victoria.ng@mhs.com>
2018-04-30 15:36:44 -0400

    added cliffs d

---
+++ R/getCliffsD.R
@@ -0,0 +1,15 @@
+#' Cliff's d
+#'
+#' Wrapper function to call effsize::cliff.delta. Returns only the Cliff's delta estimate and its confidence intervals. For further details, see original function.
+#' @param col a numeric vector with data values
+#' @param g numeric; a number specifying the number of digits to round to.
+#' @export getCliffsD
+
+# get cliff's ds
+getCliffsD <- function(col, g){
+  cd <- cliff.delta(col, g)
+  cdRes <- c(cd$estimate, cd$conf.int )
+  names(cdRes) <- c("CliffsD", 'LowCI', 'HiCI')
+  return(cdRes)
+}
+
