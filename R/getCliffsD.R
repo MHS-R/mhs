7c3cd82063871f82d1e57477223a6af52fbd650f
Victoria Ng <victoria.ng@mhs.com>
2018-05-09 10:51:41 -0400

    changed cliffsd

--- R/getCliffsD.R
+++ R/getCliffsD.R
@@ -8,8 +8,14 @@
 # get cliff's ds
 getCliffsD <- function(col, g) {
     cd <- cliff.delta(col, g)
-    cdRes <- c(cd$estimate, cd$conf.int)
-    names(cdRes) <- c("CliffsD", "LowCI", "HiCI")
+    cdRes <- data.frame(cliffD=cd$estimate, lowCI=cd$conf.int[1], highCI = cd$conf.int[2],
+                        size=as.character(cd$magnitude))
+    cdRes$signif <- ifelse(cdRes$lowCI*cdRes$highCI < 0, '', '*')
+    cdRes <- within(cdRes, {
+      cliffD <- round(cliffD, 2)
+      lowCI <- round(lowCI, 2)
+      highCI <- round(highCI, 2)
+    })
     return(cdRes)
 }
 
